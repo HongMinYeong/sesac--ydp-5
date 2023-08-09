b65deb84d3ea3b2366beee7306ced977d7e07812
HongMinYeong <minyung1240@daum.net>
2023-08-09 15:14:09 +0900

    Study : 노드 모듈 불러오기 & 사용하기

--- 08-node/app2.js
+++ 08-node/app2.js
@@ -1,15 +1,11 @@
-//math 모듈 불러와 사용
+//math2 모듈 불러와 사용
 
-//math 모듈 불러오기
-const math = require('./math2');
+//math2 모듈 불러오기
+const math2 = require('./math2');
 
-// console.log(math);
-console.log(math, typeof math);
+// console.log(math2);
+console.log(math2, typeof math2); //현재 math2는 객체
 //{ add: [Function: add], E: 2.718, PI: 3.141592 } object
-console.log(math.PI); //3.141592
-console.log(math.E); //2.718
-console.log(math.add(math.PI, math.E));
-
-console.log('substract함수', math.substract(6, 3));
-console.log('multiply함수', math.multiply(6, 3));
-console.log('divide함수', math.divide(6, 3));
+console.log(math2.PI); //3.141592
+console.log(math2.E); //2.718
+console.log(math2.add(math2.PI, math2.E));
--- 08-node/math2.js
+++ 08-node/math2.js
@@ -1,22 +1,16 @@
 //하나의 모듈 파일에 여러 개 만들기
+// 자주 사용할 함수와 변수를 정의한 파일
 const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a % b;
 const E = 2.718;
 const PI = 3.141592;
 
-//case 1
-//객체로 만들어서 내보냄
-//키와 value가 같으면 밑에처럼 해도됨
-module.exports = {
-  add,
-  E,
-  PI,
-  substract,
-  multiply,
-  divide,
-};
+//case 1. 객체로 감싸서 내보내기
+// module.exports = {
+//   add,
+//   E,
+//   PI,
+// };
+////////////////////////////////////////////////////////
 //obj에서 key,value가 동일할 때 생략 가능 ~
 // module.exports = {
 //   add: add,
@@ -24,12 +18,13 @@ module.exports = {
 //   E: E,
 // };
 
-// case 2
+/////////////////////////////////////////////////////////////////
+// case 2. 하나씩 내보내기 -> 이렇게 보내도 객체로 저장된다.
 // module.exports.add = add;
 // module.exports.E = E;
 // module.exports.PI = PI;
-
-//case 2 생략
-// exports.add = add;
-// exports.E = E;
-// exports.PI = PI;
+////////////////////////////////////////////////////////
+//case 2. 생략
+exports.add = add;
+exports.E = E;
+exports.PI = PI;
