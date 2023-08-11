75585c3dad768c2ba3bb1ae42aa9c258f7b41fdf
HongMinYeong <minyung1240@daum.net>
2023-08-11 14:48:50 +0900

    Study : ejs 활용

--- 08-express/app.js
+++ 08-express/app.js
@@ -5,6 +5,10 @@ const PORT = 8080;
 app.set('view engine', 'ejs'); //express에서 사용할 템플릿 엔진 종류(ejs) 등록
 app.set('views', './views'); //템플릿 엔진 파일을 저장할 위치 등록
 
+//(임시) 데이터베이스에서 가져온 회원 정보(id,pw)
+const idFromDB = 'banana';
+const pwFromDB = '1234qwer';
+
 //app.get(경로, 해당 경로로 들어왔을 때 실행할 함수 )
 //루트경로로 들어왔을때 함수 실행
 //'/' : 서버 주소 : 포트번호 (Localhost:8080/)
@@ -13,16 +17,25 @@ app.get('/', function (req, res) {
   //   res.send('<h1>@@@@Hello! Express!!!!@@@@</h1>');
   ////////////////////////////////////////////////////////////////////////
   //res.render(ejs_filename) : ejs file 이름을 찾아서 응답
-  res.render('index'); //index라는 ejs파일을 응답으로 보여줌
+  res.render('index', {
+    userId: idFromDB,
+    userPw: pwFromDB,
+    btns: ['사과', '오렌지', '키위'],
+    me: {
+      name: 'minong',
+      msg: '저는 저입니다. 반갑습니다!!',
+    },
+    isLogin: false,
+  }); //index라는 ejs파일을 응답으로 보여줌
 });
 
-// // '/sesac' 경로(서버주소 : 포트번호/sesac)로 들어왔을 때 "새싹 영등포캠퍼스 5기 수업중" 메세지 보이기
-// //http://localhost:8080/sesac
+// '/sesac' 경로(서버주소 : 포트번호/sesac)로 들어왔을 때 "새싹 영등포캠퍼스 5기 수업중" 메세지 보이기
+//http://localhost:8080/sesac
 // app.get('/sesac', function (req, res) {
 //   res.send('<h1>@@@@새싹 영등포캠퍼스 5기 수업중@@@@</h1>');
 // });
 
-// //서버가 실행할 PORT 지정하고, 실행했을 때 콘솔로그를 찍음
+//서버가 실행할 PORT 지정하고, 실행했을 때 콘솔로그를 찍음
 app.listen(PORT, function () {
   console.log(`server listening on ${PORT} port`);
 });
