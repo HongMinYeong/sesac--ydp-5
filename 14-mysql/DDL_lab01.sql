0f73af52ba0f4bcd8fa984381063ec9d1a9a7667
HongMinYeong <minyung1240@daum.net>
2023-08-25 15:05:46 +0900

    Practice : 추가 실습

--- 14-mysql/DDL_lab01.sql
+++ 14-mysql/DDL_lab01.sql
@@ -163,3 +163,78 @@ DESC USER;
 DESC AUTHORS;
 DESC BOOKS;
 DESC ORDERS;
+
+------------------------------------------------------------------------------------------------------------------------------
+-- < 실습 > 
+-- departments와 employees 테이블을 생성하고 데이터를 입력해주세요.
+-- 아래 문항에 맞는 SELECT 문을 작성해주세요. 
+
+use sesac;
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  location VARCHAR(50)
+);
+DESC departments;
+
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  age INT,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+DESC employees;
+
+INSERT INTO departments (id, name, location)
+VALUES
+  (1, 'Sales', 'New York'),
+  (2, 'Marketing', 'San Francisco'),
+  (3, 'Engineering', 'Seattle');
+
+INSERT INTO employees (id, name, age, department_id)
+VALUES
+  (1, 'John Doe', 25, 1),
+  (2, 'Jane Smith', 30, 1),
+  (3, 'Bob Johnson', 40, 2),
+  (4, 'Alice Lee', 35, 3),
+  (5, 'Tom Wilson', 28, 3);
+  
+SELECT * from departments;
+SELECT * from employees;
+
+-- < 풀이 > 
+-- 1. 모든 직원을 직원 테이블에 나열합니다.
+SELECT * FROM EMPLOYEES
+
+-- 2. 나이순으로 직원 테이블에 있는 모든 직원을 나이순(내림차순)으로 나열합니다.
+SELECT * FROM EMPLOYEES ORDER BY AGE DESC;
+-- 3. 직원 테이블에 30세 이상인 직원의 이름과 나이를 나열합니다.
+SELECT NAME,AGE FROM EMPLOYEES WHERE AGE >=30;
+
+-- 4. 영업부에서 근무하는 직원의 이름과 부서 ID를 직원 표에 나열합니다.
+SELECT NAME,DEPARTMENT_ID FROM EMPLOYEES WHERE DEPARTMENT_ID = 1;
+
+-- 5. 엔지니어링 부서에 근무하고 30세 미만인 직원의 이름과 나이를 직원 테이블에 나열합니다.
+SELECT NAME,AGE FROM EMPLOYEES WHERE DEPARTMENT_ID=3 AND AGE <30;
+-- 6. 직원 테이블에서 직원 수를 계산합니다.
+SELECT COUNT(*) FROM EMPLOYEES;
+
+-- 7. 직원 테이블에서 각 부서의 직원 수를 계산합니다.
+SELECT DEPARTMENT_ID,COUNT(*) FROM EMPLOYEES GROUP BY DEPARTMENT_ID;
+
+-- 8. 직원 평균 나이를 계산합니다.
+SELECT AVG(AGE) FROM EMPLOYEES;
+
+-- 9. 부서별 평균 나이를 계산합니다.
+SELECT DEPARTMENT_ID,AVG(AGE) FROM EMPLOYEES GROUP BY DEPARTMENT_ID;
+
+-- 10. 부서 테이블에서 지역 컬럼의 두번째 글자가 e인 부서를 계산합니다.
+SELECT * FROM DEPARTMENTS WHERE LOCATION LIKE '_E%';
+
+-- 11. 부서 테이블에서 지역 컬럼에 공백이 들어가는 부서를 계산합니다.
+
+SELECT * FROM DEPARTMENTS WHERE LOCATION LIKE '% %';
+-- 12. 직원 테이블에서 이름 컬럼에서 마지막 글자가 n인 사원을 계산합니다.
+
+SELECT * FROM EMPLOYEES WHERE NAME LIKE '_%N';
