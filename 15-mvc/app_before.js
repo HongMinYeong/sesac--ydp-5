36919ba4016d64f7868ba73b5a5e0d103b53836d
HongMinYeong <minyung1240@daum.net>
2023-08-28 18:26:20 +0900

    Fix: app.set 수정

--- 15-mvc/app_before.js
+++ 15-mvc/app_before.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.set('/views', 'views');
+app.set('views', '/views');
 
 app.use('/views', express.static(__dirname + '/views'));
 app.use('/static', express.static(__dirname + '/static'));
