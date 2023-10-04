c80f6386cbeb2f4041254375586a9cb0e913f60e
HongMinYeong <minyung1240@daum.net>
2023-10-04 14:36:03 +0900

    Study: reduce()내장메서드

--- 04-javascript/index8.js
+++ 04-javascript/index8.js
@@ -205,3 +205,24 @@ for (let key in me) {
   //key : key
   //value : me[key]
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//reduce()
+//:배열의 각 요소에 대해서 주어진 리듀서(reducer) 함수를 실행하고, '하나의 결과값' 반환
+
+//reduce((acc,cur) =>{ ... })
+//reduce((acc,cur) =>{ ... },initValue)
+// -acc : 누적되는 값
+// -cur : 현재 요소
+
+// const number = [1, 2, 3, 4, 5];
+// const result = number.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// });
+// console.log(result); //15
+const number = [1, 2, 3, 4, 5];
+const initialValue = 100; //초기값 설정
+const result = number.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, initialValue);
+console.log(result); //115
