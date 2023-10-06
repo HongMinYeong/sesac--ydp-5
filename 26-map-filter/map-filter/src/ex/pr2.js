6f5e65c0efc90732b3ab9429cb07088ff2b5b4dd
HongMinYeong <minyung1240@daum.net>
2023-10-06 13:46:43 +0900

    Practice: React 선택실습 정답코드

--- 26-map-filter/map-filter/src/ex/pr2.js
+++ 26-map-filter/map-filter/src/ex/pr2.js
@@ -1,73 +1,145 @@
 import React, { useState } from 'react';
 
 export default function Pr2() {
-  const [inputName, setInputName] = useState('');
-  const [inputTitle, setInputTitle] = useState('');
-  const [search, setSearch] = useState('');
-  const [data, setData] = useState([]);
-  const addData = () => {
-    const newData = data.concat({
-      id: data.length,
-      name: inputName,
-      title: inputTitle,
+  //state 하나로 writer,title input을 동시에 관리 !
+  const [inputs, setInputs] = useState({
+    writer: '',
+    title: '',
+    search: '',
+  });
+  const { writer, title, search } = inputs; //구조분해
+  const [comments, setComments] = useState([
+    {
+      writer: '미녕',
+      title: '하이',
+    },
+  ]);
+  const [searchType, setSearchType] = useState('writer');
+  const [result, setResult] = useState([]);
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value, //ex. writer:d
     });
-    setData(newData);
-    setInputTitle('');
-    setInputName('');
   };
-  const searchData = (targetName) => {
-    search = data.filter((value) => value.name === targetName);
-    setSearch(search);
+  const addComment = () => {
+    const newComment = {
+      // writer:writer,
+      // title:title,
+      writer,
+      title,
+    };
+    setComments([...comments, newComment]); //기존 + 추가할거 하나
+    //input 초기화
+    setInputs({
+      // ...inputs,
+      writer: '',
+      title: '',
+    });
+  };
+  const selectSearchType = (e) => {
+    setSearchType(e.target.value);
+  };
+
+  const searchComment = () => {
+    const searchResult = comments.filter((cmt) => {
+      //cmt :{title:xxx,writer:xxx}
+      if (!cmt[searchType].includes(search)) {
+        //검색결과가 없다면; null
+        return null;
+      } else {
+        return cmt;
+      }
+    });
+    setResult(searchResult);
+    setInputs({
+      ...inputs,
+      search: '',
+    });
   };
   return (
     <div>
-      <fieldset>
-        <label for="writer">작성자 :</label>
+      <form>
+        <label htmlFor="writer">작성자:</label>
         <input
+          id="writer"
           type="text"
-          value={inputName}
-          placeholder="작성자"
-          onChange={(e) => {
-            setInputName(e.target.value);
-          }}
+          name="writer"
+          value={writer}
+          onChange={onChange}
         />
-        <label for="title"> 제목 : </label>
+        <label htmlFor="title">제목:</label>
         <input
+          id="title"
           type="text"
-          value={inputTitle}
-          onChange={(e) => {
-            setInputTitle(e.target.value);
-          }}
+          name="title"
+          value={title}
+          onChange={onChange}
         />
-        <button onClick={addData}> 작성</button>
-      </fieldset>
-      <br />
-      <br />
-      <select>
-        <option value={inputName} selected>
-          작성자
-        </option>
-        <option value={inputTitle}>제목</option>
-      </select>
-      <input type="text" placeholder="검색어" />
-      {data.map((value) => {
-        <button onClick={() => searchData(value.name)}>검색</button>;
-      })}
-      <div>
-        <tr>
-          <td>번호</td>
-          <td>제목</td>
-          <td>작성자</td>
-        </tr>
+        <button type="button" onClick={addComment}>
+          작성
+        </button>
+      </form>
 
-        {data.map((value) => (
+      <form>
+        <select name="type" value={searchType} onChange={selectSearchType}>
+          <option value="writer">작성자</option>
+          <option value="title">제목</option>
+        </select>
+
+        <input
+          type="text"
+          name="search"
+          placeholder="검색어"
+          value={search}
+          onChange={onChange}
+        />
+        <button type="button" onClick={searchComment}>
+          검색
+        </button>
+      </form>
+
+      <h3>전체 댓글 목록</h3>
+      <table border={1} style={{ margin: '30px auto', width: '500px' }}>
+        <thead>
           <tr>
-            <td>{data.length}</td>
-            <td>{value.title}</td>
-            <td>{value.name}</td>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
           </tr>
-        ))}
-      </div>
+        </thead>
+        <tbody>
+          {comments.map((cmt, idx) => (
+            <tr key={idx + 1}>
+              <td>{idx + 1}</td>
+              <td>{cmt.title}</td>
+              <td>{cmt.writer}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>댓글 검색 결과</h3>
+      <table border={1} style={{ margin: '30px auto', width: '500px' }}>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((cmt, idx) => (
+            <tr key={idx + 1}>
+              <td>{idx + 1}</td>
+              <td>{cmt.title}</td>
+              <td>{cmt.writer}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
