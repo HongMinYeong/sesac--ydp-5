4501bfb6b4ab42db01845e0f022111ec4a3c4aa3
HongMinYeong <minyung1240@daum.net>
2023-10-11 15:23:08 +0900

    Study:sass

---
+++ 29-style/src/App.js
@@ -0,0 +1,18 @@
+import CssModuleComponents from './CssModuleComponents';
+import SassComponent from './SassComponent';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React style</h1>
+      <h2>CSS Module</h2>
+      <CssModuleComponents />
+
+      <h2>SASS</h2>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
