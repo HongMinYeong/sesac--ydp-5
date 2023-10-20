4ed30bd5b66f7d405969225e551e42f2406dbdd0
HongMinYeong <minyung1240@daum.net>
2023-10-20 15:12:27 +0900

    Study: typescript_functypes

--- 32-typescript/02_onlyTsType.ts
+++ 32-typescript/02_onlyTsType.ts
@@ -87,3 +87,137 @@ enum Cafe2 {
 }
 
 console.log(Auth2[5]); // 출력 가능
+// console.log(Cafe2[1]); // 출력 불가능
+
+////////////////////////////////////////////////
+//1. interface
+interface Student {
+  name: string;
+  isPassed: boolean;
+}
+
+const student1: Student = {
+  name: 'jinheyong',
+  isPassed: true,
+  // addr:'seoul',
+};
+
+const student2: object = {
+  name: 'jinheyong',
+  isPassed: true,
+  addr: 'seoul',
+};
+
+//interface 상속
+//A+,A,B,C,D,F
+//ype Score = 'A+' | 'A' | 'B' | 'C' | 'D' | 'F';
+enum Score {
+  Aplus = 'A+',
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  F = 'F',
+}
+interface BaseballClub extends Student {
+  position: string;
+  height: number;
+  readonly backNumber?: number; // ?: 있어도 되고 없어도 됨
+  // [grade: number]: string;
+  [grade: number]: Score;
+}
+
+const otani: BaseballClub = {
+  name: 'otani',
+  isPassed: true,
+  position: 'pitcher',
+  height: 193,
+  // 1: 'A+', //학년 : 점수
+  1: Score.A,
+  //2: 'NP', //type error
+  backNumber: 17,
+};
+console.log(otani);
+otani.position = '투수';
+otani['height'] = 200;
+// otani.height = 210;
+//otani.backNumber = 16; //error: backNumber readonly
+console.log(otani);
+
+type Bp1 = 500 | 700 | 1000;
+enum Bp2 {
+  SM = 500,
+  MD = 700,
+  LG = 1000,
+}
+const width1: Bp1 = 500;
+const width2: Bp2 = Bp2.SM;
+
+//교차 타입 : 두개 이상의 타입을 합치는 것
+interface Toy {
+  name: string;
+  start(): void;
+}
+interface Car {
+  name: string;
+  color: string;
+  price: number;
+}
+
+// const toyCar: Toy & Car = {
+//   name: 'tayo',
+//   start() {
+//     console.log('출발~~');
+//   },
+//   color: 'blue',
+//   price: 5000,
+// };
+type ToyCar = Toy & Car;
+const toyCar: ToyCar = {
+  name: 'tayo',
+  start() {
+    console.log('출발~~');
+  },
+  color: 'blue',
+  price: 5000,
+};
+
+//2.type
+type Gender = 'F' | 'M';
+type Person = {
+  name: string;
+  age?: number;
+  like?: string[]; //원소가 문자열인 배열
+  // gender: string;
+  gender: Gender;
+};
+
+let daniel: Person = {
+  name: 'daniel',
+  age: 20,
+  gender: 'F',
+  // gender: 'f', //gender:Gender일때 type error
+  // like: ['minji', 'hani'], //없어도 그만!
+};
+interface Game {
+  title: string;
+  price: number;
+  desc?: string;
+  category: '액션' | '롤플레잉';
+  platform: '모바일' | '콘솔';
+}
+let heroGema_A: Game = {
+  title: 'DC 언체인드',
+  price: 50000,
+  desc: 'DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!',
+  category: '액션',
+  platform: '모바일',
+};
+
+let heroGema_B: Game = {
+  title: 'MARVEL 퓨처파이트',
+  price: 65000,
+  // desc: 'DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!',
+  category: '롤플레잉',
+  platform: '모바일',
+};
