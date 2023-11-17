d5012babd0022cfe96a24ad14bf20b27831cd8f4
HongMinYeong <minyung1240@daum.net>
2023-11-17 14:43:48 +0900

    fix: 귓속말 나자신은 제외
    귓속말 보내고고select option 초기화

--- 35-socket.io/server.js
+++ 35-socket.io/server.js
@@ -22,48 +22,41 @@ function updateList() {
 //최초 연결
 //io.on(): socket 관련한 통신 작업으 처리
 io.on('connection', (socket) => {
-  //connection 이벤트는 클라이언트가 접속했을 때 발생
-  //콜백 함수의 인자로 소켓 객체를 제공
-  console.log(`서버 연결되었습니다.:${socket.id}`); //콜백으로 클라이언트 소켓정보를 받아옴
-  let sockets = [];
-  sockets.push(socket.id);
-  console.log(sockets);
+  // connection 이벤트는 클라이언트가 접속했을 때 발생
+  // 콜백 함수의 인자로 소켓 객체를 제공
 
-  //[실습3-2]
-  //emit() from server
-  // - socket.emit(event_name,data) : 해당 클라이언트에게만 이벤트, 데이터를 전송
+  console.log('서버 연결 완료 :: ', socket.id);
+  // socket.id: 소켓 고유 아이디 (브라우저 탭 단위)
 
-  // - io.emit(event_name,data): 서버에 접속된 모든 클라이언트 전송
-  socket.on('setNick', async (nick) => {
+  // [실습1]
+  //   socket.on('hello', (data) => {
+  //     console.log(`${data.who} : ${data.msg}`);
+  //     socket.emit('helloKr', { who: 'hello', msg: '안녕!!!!' });
+  //   });
+
+  // [실습3] 채팅창 입장 안내 문구
+  //   io.emit('notice', `${socket.id} 님이 입장하셨습니다.`);
+
+  // [실습3-2] 채팅창 입장 문구 socket.id -> nickname
+  // emit() from server
+  // - socket.emit(event_name, data): 해당 클라이언트에게만 이벤트, 데이터를 전송
+  // - io.emit(event_name, data): 서버에 접속된 모든 클라이언트 전송
+  socket.on('setNick', (nick) => {
     console.log(`닉네임 설정 완료 :: ${nick} 님 입장`);
+
+    // 프론트에서 입력한 nick이 nickObjs 객체에 존재하는지 검사
+    // 이미 존재; error 이벤트 + '이미 존재하는 닉네임 입니다' => 클라이언트; error 이벤트 받으면 alert 띄우기
+    // 새 닉네임; notice 이벤트 + ${nick} 님이 입장하셨습니다.
     if (Object.values(nickObjs).indexOf(nick) > -1) {
+      // 이미 존재하는 닉네임이 있음
       socket.emit('error', '이미 존재하는 닉네임입니다.');
     } else {
+      // 새로운 닉네임
       nickObjs[socket.id] = nick;
-      console.log('접속 유저 목록 ::', nickObjs);
-      io.emit('joinRoom', `${nick}님이 입장하셨습니다.`);
-      socket.emit('entrySuccess', nick); //해당 소켓ㅅ 데이ㅓ터 전송
+      console.log('접속 유저 목록 :: ', nickObjs);
+      io.emit('notice', `${nick} 님이 입장하셨습니다.`); // 전체 공지
+      socket.emit('entrySuccess', nick); // 해당 소켓 데이터 전송
       updateList();
-    }
-    // const nickObjs = {}; //{socket.id:nick1,socket.id:nick2}
-  });
-  socket.on('joinRoom', async (name, id, cb) => {
-    try {
-      console.log('클라이언트', id);
-      const user = sockets.includes(id);
-      console.log('user는', user);
-      if (user) {
-        const welcomeMessage = {
-          chat: `${name}님이 입장하셨습니다.`,
-          msg: 'system',
-        };
-        console.log('환영', welcomeMessage);
-
-        cb({ isOk: true, data: welcomeMessage });
-      }
-    } catch (error) {
-      console.log('에러당', error);
-      cb({ isOk: false, error: error.message });
     }
   });
 
@@ -73,8 +66,15 @@ io.on('connection', (socket) => {
   socket.on('send', (data) => {
     console.log('send 이벤트로 받은 data:: ', data);
     // send 이벤트로 받은 data::  { myNick: '민민al', msg: 'gg' }
-    //전체로 보냄
-    io.emit('newMessage', { nick: data.myNick, msg: data.msg });
+    // [실습 5]
+    // 디엠인지 아닌지 구분해서
+    //io.to(소켓아이디).emit(event_name,data) : 소켓 아이디에 해당하는 클라이언트에게만 전송
+    if (data.dm === 'all') {
+      io.emit('newMessage', { nick: data.myNick, msg: data.msg });
+    } else {
+      io.to(data.dm).emit('dm', { nick: data.myNick, msg: data.msg });
+      io.to(socket.id).emit('dm', { nick: data.myNick, msg: data.msg });
+    }
   });
 
   //   socket.on('meow', (data) => {
